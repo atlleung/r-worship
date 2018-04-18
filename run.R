036a6a721b3f222b91f31c89ca8d702928296ab7
andrewleung93 <andrewleung93@gmail.com>
2018-04-18 17:35:53 -0400

    heroku attempt2

--- run.R
+++ run.R
@@ -7,12 +7,20 @@
 #    http://shiny.rstudio.com/
 #
 
+my_packages = c("shiny", "httr", "jsonlite")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
+
 library(shiny)
-library(tidyverse)
+# library(tidyverse)
 library(httr)
 library(jsonlite)
-
-
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
