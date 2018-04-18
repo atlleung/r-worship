8b5322a70fb1d53510402d5f8030e3562b77c018
andrewleung93 <andrewleung93@gmail.com>
2018-04-18 17:40:36 -0400

    heorku build

--- run.R
+++ run.R
@@ -261,4 +261,9 @@ server <- function(input, output, session) {
 }
 
 # Run the application
-shinyApp(ui = ui, server = server)
+port <- Sys.getenv('PORT')
+shiny::runApp(
+  appDir = getwd(),
+  host = '0.0.0.0',
+  port = as.numeric(port)
+)
